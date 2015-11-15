7032067f239112fef2a622013e59f044de0d297a
enlore <n.e.lorenson@gmail.com>
2015-11-15 11:30:30 -0600

    Post css

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,28 @@
-"use strict";
 /* jshint laxcomma: true */
-require("colors");
-var gulp                = require("gulp");
-var gChmod              = require("gulp-chmod");
-var gWeb                = require("gulp-connect");
 
-const autoprefixer      = require("gulp-html-autoprefixer");
-const gIf               = require("gulp-if");
+"use strict";
+
+require("colors");
+const gulp                = require("gulp")
+    , gChmod              = require("gulp-chmod")
+    , gWeb                = require("gulp-connect")
+    , gIf                 = require("gulp-if")
+
+const gPostCSS            = require("gulp-postcss")
+    , postImport          = require("postcss-import")
+    , htmlPrefixer        = require("gulp-html-autoprefixer")
+    , autoPrefixer        = require("gulp-autoprefixer")
 
 const config = {
     permissionBits: 664,
     autoprefix: process.env.PREFIX === "true" ? true : false,
+
+    /**
+     * Check out the docs for the postcss-import processor
+     * https://github.com/postcss/postcss-import#usage
+     */
+    importCSS: process.env.IMPORT_CSS === "true" ? true : false,
+
     server: {
         reload: process.env.RELOAD === "false" ? false : true,
         port: process.env.PORT || 3000
@@ -19,10 +31,12 @@ const config = {
 
 const paths = {
     src: { // src files if working with transcompiled stuff
-        root: "src/**/*",
-        html: "src/**/*.html",
-        app: "src/app/**/*",
-        layout: "src/index.html"
+        root    : "src/**/*",
+        html    : "src/**/*.html",
+        app     : "src/app/**/*",
+        layout  : "src/index.html",
+        css     : "src/css/**/*.css",
+        cssMain : "src/css/main.css"
     },
 
     vendor: { // vendors stuff/lib/dependencies
@@ -32,6 +46,8 @@ const paths = {
 
     dest: { // output dirs
         root: "dist",
+        css: "dist/css",
+        js: "dist/js",
         vendor: {
             js: "dist/js/vendor",
             css: "dist/css/vendor"
@@ -57,11 +73,33 @@ const tasks = ["vendorJs", "vendorCss", "appFiles", "serve", "watch"]
 gulp.task("default", tasks);
 
 gulp.task("appFiles", function () {
-    const autopref = autoprefixer(autoprefixConfig)
+    const htmlPref = htmlPrefixer(autoprefixConfig)
+        , autoPref = autoPrefixer(autoprefixConfig)
 
     gulp.src(paths.src.html)
-        .pipe(gIf(config.autoprefix, autopref)) // conditionally do autoprefixing of inline css in html files
+        .pipe(gIf(config.autoprefix, htmlPref)) // conditionally do autoprefixing of inline css in html files
         .pipe(gulp.dest(paths.dest.root));
+
+    /**
+     * if you're using @import directives to modularize your css, do this via
+     * the IMPORT_CSS env flag see the above comment in the cofig object for
+     * notes on using @import directives
+     */
+    if (config.importCSS) {
+        gulp.src(paths.src.cssMain)
+            .pipe(gPostCSS([ postImport() ]))
+            .pipe(gIf(config.autoprefix, autoPref)) // still conditionally using the prefixer
+            .pipe(gulp.dest(paths.dest.css))
+
+    /**
+     * if !importCSS, the build process will consuming all css files
+     * individually and write them out to the dist/css folder individually
+     */
+    } else {
+        gulp.src(paths.src.css)
+            .pipe(gIf(config.autoprefix, autoPref))
+            .pipe(gulp.dest(paths.dest.css))
+    }
 })
 
 if (config.server.reload) {
@@ -75,6 +113,7 @@ gulp.task("watch",  function () {
     gulp.watch(paths.vendor.js,     ["vendorJs"]);
     gulp.watch(paths.vendor.css,    ["vendorCss"]);
     gulp.watch(paths.src.html,      ["appFiles"])
+    gulp.watch(paths.src.css,       ["appFiles"])
 
     if (config.server.reload) {
         gulp.watch(paths.dest.root, ["reload"])
